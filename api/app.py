beda9ac3f644a4b8407bad66ecc69fab637f8d88
Akshat Pandey <akshatpandey660@gmail.com>
2023-05-12 23:04:58 +0530

    added README.md

--- api/app.py
+++ api/app.py
@@ -1,37 +1,8 @@
 from flask import Flask, request
 from flask_cors import CORS
-# from elasticsearch import Elasticsearch
-
-# es = Elasticsearch(hosts=["http://127.0.0.1:9200"])
-
-# print(f"Connected to ElasticSearch cluster `{es.info().body['cluster_name']}`")
 
 app = Flask(__name__)
 CORS(app)
-
-MAX_SIZE = 15
-
-
-# @app.route("/search")
-# def search_autocomplete():
-#     query = request.args.get("q", "").lower()
-#     tokens = query.split(",")
-#     clauses = [
-#         {
-#             "span_multi": {
-#                 "match": {"fuzzy": {"name": {"value": i, "fuzziness": "AUTO"}}}
-#             }
-#         }
-#         for i in tokens
-#     ]
-#     payload = {
-#         "bool": {
-#             "must": [{"span_near": {"clauses": clauses, "slop": 0, "in_order": False}}]
-#         }
-#     }
-
-#     resp = es.search(index="cars", query=payload, size=MAX_SIZE)
-#     return [result['_source']['name'] for result in resp['hits']['hits']]
 
 
 try:
--- api/model/platform.py
+++
@@ -1,5 +0,0 @@
-class platform():
-    def __init__(self, name, project_count, homepage):
-        self.name = name
-        self.project_count = project_count
-        self.homepage = homepage
